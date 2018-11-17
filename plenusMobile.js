f05841142725d6ce72f344edb57b96b9a2b3b899
ismaelAlfra <39815465+ismaelAlfra@users.noreply.github.com>
2018-11-17 01:25:15 -0200

    Create plenusMobile.js

---
+++ plenusMobile.js
@@ -0,0 +1,22 @@
+let opcoes = document.querySelector(".opcoes");
+let txt = opcoes.querySelectorAll("h1");
+console.log(txt);
+
+function select(){
+    for(let i = 0; i < txt.length;i++){
+        if(txt[i].addEventListener("click", function(){
+            txt[i].classList.add("selecionado");
+
+            for(x of txt){
+                if(x.innerHTML != txt[i].innerHTML){
+                    x.classList.remove("selecionado");
+                }
+            }
+            
+        })){
+
+        }
+    }
+}
+
+select();
