cba19a0c733e15f557b70adaa7ad330d3ede0d36
ismaelAlfra <39815465+ismaelAlfra@users.noreply.github.com>
2018-11-17 21:26:22 -0200

    Update plenusMobile.js

--- plenusMobile.js
+++ plenusMobile.js
@@ -1,11 +1,21 @@
+let subOpcoes = document.querySelectorAll(".sub-opcoes");
 let opcoes = document.querySelector(".opcoes");
 let txt = opcoes.querySelectorAll("h1");
 console.log(txt);
+console.log(subOpcoes);
 
 function select(){
     for(let i = 0; i < txt.length;i++){
         if(txt[i].addEventListener("click", function(){
             txt[i].classList.add("selecionado");
+            for(y of subOpcoes){
+                if(y.id == txt[i].innerHTML){
+                    y.classList.add("abrir");
+            }
+                else{
+                    y.classList.remove("abrir");  
+                }
+        }
 
             for(x of txt){
                 if(x.innerHTML != txt[i].innerHTML){
